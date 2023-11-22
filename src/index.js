583e71424791b924648d79501bcd32df40064109
Anna Tsepilova <virgo.tsav@gmail.com>
2023-11-22 23:24:17 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+//   iziToast.show({
+//     title: 'Error',
+//     message: 'Oops! Something went wrong! Try reloading the page!',
+//     position: 'topRight',
+//     color: 'red',
+//   });
